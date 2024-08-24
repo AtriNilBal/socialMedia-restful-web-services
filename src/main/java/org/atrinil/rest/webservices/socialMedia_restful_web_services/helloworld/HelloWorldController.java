b8ecf0cdd8142cad5b8ec117b122ff1c3f01d18f
ATRI NIL BAL <atrinilbal@ATRIs-MacBook-Air.local>
2024-08-24 12:41:14 +0530

    add i18n for a get call

--- src/main/java/org/atrinil/rest/webservices/socialMedia_restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/org/atrinil/rest/webservices/socialMedia_restful_web_services/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package org.atrinil.rest.webservices.socialMedia_restful_web_services.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 //REST API
 @RestController
 public class HelloWorldController  {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     // url - /hello-world
     //return String "Hello world"
@@ -23,6 +33,12 @@ public class HelloWorldController  {
     @GetMapping(value = "/hello-world/greet/{user}")
     public HelloWorldBean greetUser(@PathVariable String user) {
         return new HelloWorldBean(String.format("Hello %s", user));
+    }
+    @GetMapping(path = "/hello-world-internationalized")
+    public String HelloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message",
+                locale);
     }
 
 }
